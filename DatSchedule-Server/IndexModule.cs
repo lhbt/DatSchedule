ad5e037884d08ead1557b06432f5d19bc786feaf
ssunkari <sr.sunkari@gmail.com>
2014-10-25 12:09:35 +0100

    Added default nancy project with asp.net hosting

---
+++ DatSchedule-Server/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿namespace DatSchedule_Server
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}

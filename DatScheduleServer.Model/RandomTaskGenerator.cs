d622a8422cad815d362c23a3083e0d152d529b85
lhbt <laurent.humbert8@gmail.com>
2014-10-26 03:10:51 +0000

    balanced the random task list

--- DatScheduleServer.Model/RandomTaskGenerator.cs
+++ DatScheduleServer.Model/RandomTaskGenerator.cs
@@ -11,7 +11,7 @@ namespace DatScheduleServer.Model
             var random = new Random();
             var randomTaskList = new List<Task>();
 
-            while (GetUniquetasks(randomTaskList).Count != 9)
+            while (GetUniquetasks(randomTaskList).Count != 9 && !ListIsBalanced(randomTaskList))
             {
                 var index = random.Next(set.Count());
                 randomTaskList.Add(set[index]);
@@ -23,6 +23,30 @@ namespace DatScheduleServer.Model
         private static List<Task> GetUniquetasks(IEnumerable<Task> randomTaskList)
         {
             return randomTaskList.Distinct().ToList();
+        }
+
+        private static bool ListIsBalanced(IEnumerable<Task> randomTaskList)
+        {
+            var taskList = randomTaskList as Task[] ?? randomTaskList.ToArray();
+
+            var threeHrsLongMeetings = GetMeetingsOfDuration(taskList, 3).ToList();
+            var betweenOneAndTwo3HrsMeetings = threeHrsLongMeetings.Count() <= 1 && threeHrsLongMeetings.Count() >= 2;
+
+            var twoHrsLongMeetings = GetMeetingsOfDuration(taskList, 2).ToList();
+            var betweenOneAndTwoHrsLongMeetings = twoHrsLongMeetings.Count() <= 1 && twoHrsLongMeetings.Count() >= 2;
+
+            var oneMeal = taskList.Count(x => x.Type == TaskType.Meal) == 1;
+
+            var leisureBreaks = taskList.Count(x => x.Type == TaskType.Leisure);
+            var betweenOneAndTwoLeisureBReaks = leisureBreaks <= 1 && leisureBreaks >= 3;
+
+            return betweenOneAndTwo3HrsMeetings && betweenOneAndTwoHrsLongMeetings && oneMeal &&
+                   betweenOneAndTwoLeisureBReaks;
+        }
+
+        private static IEnumerable<Task> GetMeetingsOfDuration(IEnumerable<Task> randomTaskList, int duration)
+        {
+            return randomTaskList.Where(x => x.Type == TaskType.Meeting && x.Duration == duration);
         }
     }
 }

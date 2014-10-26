f3f870bcab76b9a087afa88bdc7d418d1ff947e8
lhbt <laurent.humbert8@gmail.com>
2014-10-26 08:16:12 +0000

    tuned values up a bit to make the game more difficult

--- DatScheduleServer.Model/GameRulesParameters.cs
+++ DatScheduleServer.Model/GameRulesParameters.cs
@@ -2,12 +2,12 @@
 {
     public class GameRulesParameters
     {
-        public static int ImpactOfSleepOnFatigue { get { return 20; }}
+        public static int ImpactOfSleepOnFatigue { get { return 18; }}
         public static int ImpactOfMealOnHunger { get { return 15; }}
         public static int ImpactOfLeisureOnStress { get { return 20; }}
 
-        public static int ImpactOfMeetingOnStress { get { return 8; }}
-        public static int ImpactOfMeetingOnHunger { get { return 5; }}
-        public static int ImpactOfMeetingOnFatigue { get { return 8; }}
+        public static int ImpactOfMeetingOnStress { get { return 10; }}
+        public static int ImpactOfMeetingOnHunger { get { return 6; }}
+        public static int ImpactOfMeetingOnFatigue { get { return 10; }}
     }
 }

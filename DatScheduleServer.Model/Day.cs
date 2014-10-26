f04e9ecab9124b2a5ac4a0105a0c86c4aa6b118b
ssunkari <sr.sunkari@gmail.com>
2014-10-26 02:08:27 +0000

    Added random tasks

--- DatScheduleServer.Model/Day.cs
+++ DatScheduleServer.Model/Day.cs
@@ -15,26 +15,10 @@ namespace DatScheduleServer.Model
 
         public List<Task> Tasks { get; set; }
 
-        private List<Task> CreateTasks()
-        {
-            //at some point should be refactored to generate a list of tasks based on an algorithm and pseudo randomness
-            return new List<Task>
-            {
-                new Task("Team Meeting", 2, TaskType.Meeting, "#33CCFF"),
-                new Task("Interview", 1, TaskType.Meeting, "#33CCFF"),
-                new Task("Big Executive Meeting", 3, TaskType.Meeting, "#33CCFF"),
-                new Task("Meal", 1, TaskType.Meal, "#FF6633"),
-                new Task("Leisure", 1, TaskType.Leisure, "#FFFF33"),
-                new Task("Swimming", 1, TaskType.Leisure, "#FFFF33"),
-                new Task("Retrospective", 2, TaskType.Meeting, "#33CCFF"),
-                new Task("Code kata", 1, TaskType.Meeting, "#33CCFF")
-            };
-        }
-
         public void Reset(int duration)
         {
             Duration = duration;
-            Tasks = CreateTasks();
+            Tasks = RandomTaskGenerator.GetUniquieSet(GameTasks.ListOfTasks);
             TimeSpent = 0;
         }
     }
--- test/DatSchduleServer.UnitTests/DealWithLeftOverTaskTests.cs
+++ test/DatSchduleServer.UnitTests/DealWithLeftOverTaskTests.cs
@@ -16,6 +16,17 @@ namespace DatScheduleServer.Tests.UnitTests
             GameRulesEnforcer.ApplyRule( new Task("Leisure", 1, TaskType.Leisure, "#FFFF33"),game);
             Assert.That(game.CurrentDay.Tasks.Any(x => !x.Scheduled));
         }
+        [Ignore]
+        [Test]
+        public void GivenIHaveFilledADayThenLeftOverTasksAreAppendedInNextDay()
+        {
+            var game = new Game();
+            GameRulesEnforcer.ApplyRule(new Task("Team Meeting", 2, TaskType.Meeting, "#33CCFF"), game);
+            GameRulesEnforcer.ApplyRule(new Task("Big Executive Meeting", 3, TaskType.Meeting, "#33CCFF"), game);
+            GameRulesEnforcer.ApplyRule(new Task("Retrospective", 2, TaskType.Meeting, "#33CCFF"), game);
+            GameRulesEnforcer.ApplyRule(new Task("Leisure", 1, TaskType.Leisure, "#FFFF33"), game);
+            Assert.That(game.CurrentDay.Tasks.Any(x => !x.Scheduled));
+        }
 
       
     }

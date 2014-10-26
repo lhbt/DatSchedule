2b096826d104ec03cbd6e03d4f1fcf656edf60b2
lhbt <laurent.humbert8@gmail.com>
2014-10-26 01:37:28 +0100

    added equality tests for the Task class

--- DatScheduleServer.Model/Task.cs
+++ DatScheduleServer.Model/Task.cs
@@ -2,6 +2,23 @@
 {
     public class Task
     {
+        protected bool Equals(Task other)
+        {
+            return Duration == other.Duration && string.Equals(Name, other.Name) && string.Equals(ColorCode, other.ColorCode) && Type == other.Type;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Duration;
+                hashCode = (hashCode*397) ^ (Name != null ? Name.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ (ColorCode != null ? ColorCode.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ (int) Type;
+                return hashCode;
+            }
+        }
+
         public Task(string name, int duration, TaskType type, string colorCode)
         {
             Name = name;
@@ -14,5 +31,13 @@
         public string Name { get; private set; }
         public string ColorCode { get; private set; }
         public TaskType Type { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Task) obj);
+        }
     }
 }
